6b14b4e15f23bd179d44b0aa9c426e77bf190417
Shenon69 <ask.tikki69@gmail.com>
2024-11-18 23:26:19 +0530

    feat: arch to fedora blog

--- src/const/data/blogs.ts
+++ src/const/data/blogs.ts
@@ -1,5 +1,12 @@
 export const blogsData = [
   {
+    title: "Arch+Hyprland to Fedora+GNOME: Why I Made the Switch",
+    link: "https://medium.com/foss-nsbm/from-arch-hyprland-to-fedora-gnome-why-i-made-the-switch-db6d4723c45b",
+    duration: "2 min read",
+    date: "Nov, 2024",
+    image: "fedora.webp"
+  },
+  {
     title: "Arch + tmux + Neovim made me lazy (In a good way)",
     link: "https://medium.com/foss-nsbm/arch-tmux-neovim-made-my-lazy-in-a-good-way-4c8c3f8cd5cf",
     duration: "2 min read",
